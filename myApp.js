63f5c4f66e60fd66bf6bcd0d08e44343538ef4cd
nastya.done <nastya.done@gmail.com>
2016-08-17 01:23:29 +0300

    add new feature

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ app.controller('myController', function ($scope) {
 
     $scope.showDataItems = function () {
         $scope.saved = localStorage.getItem('allItems');
-        $scope.items = (localStorage.getItem('allItems')!==null) ? JSON.parse($scope.saved) :
+        $scope.items = (localStorage.getItem('allItems') !== null) ? JSON.parse($scope.saved) :
             [{
                 name: 'First item with custom name',
                 comments: [{
@@ -24,41 +24,41 @@ app.controller('myController', function ($scope) {
                 }]
             }];
         localStorage.setItem('allItems', JSON.stringify($scope.items));
-    $scope.selected = 1;
+        $scope.selected = 1;
 
-    $scope.select = function (index) {
-        $scope.selected = index;
-    };
+        $scope.select = function (index) {
+            $scope.selected = index;
+        };
 
-    $scope.addItemField = function () {
-        $scope.items.push({
-            name: $scope.newName,
-            comments: []
-        });
-        localStorage.setItem('allItems', JSON.stringify($scope.items));
-
-    };
-    $scope.removeItem = function (index) {
-        $scope.items.splice(index, 1);
-        localStorage.setItem('allItems', JSON.stringify($scope.items));
-    };
-
-    $scope.onClickInputField = function ($event) {
-        $event.target.select();
-    };
-
-    $scope.addComment = function (element) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            $scope.items[element].comments.push({
-                img: '',
-                text: $scope.newComment
+        $scope.addItemField = function () {
+            $scope.items.push({
+                name: $scope.newName,
+                comments: []
             });
-            $scope.newComment = '';
-        }
-        localStorage.setItem('allItems', JSON.stringify($scope.items));
-    };
+            $scope.selected = $scope.items.length - 1;
+            localStorage.setItem('allItems', JSON.stringify($scope.items));
 
+        };
+        $scope.removeItem = function (index) {
+            $scope.items.splice(index, 1);
+            localStorage.setItem('allItems', JSON.stringify($scope.items));
+        };
+
+        $scope.onClickInputField = function ($event) {
+            $event.target.select();
+        };
+
+        $scope.addComment = function (element) {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+                $scope.items[element].comments.push({
+                    img: '',
+                    text: $scope.newComment
+                });
+                $scope.newComment = '';
+            }
+            localStorage.setItem('allItems', JSON.stringify($scope.items));
+        };
 
     };
 });
